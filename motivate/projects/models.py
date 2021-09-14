4e5f1e2fa3029ae5ebb50fbe924c84b3987bafd9
iusharnina@gmail.com <zjulia679@gmail.com>
2021-09-14 20:28:11 +1000

    Added users

--- motivate/projects/models.py
+++ motivate/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Project(models.Model):
@@ -8,13 +9,24 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     project = models.ForeignKey(
-        'Project',on_delete=models.CASCADE,related_name='pledges'
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
     )
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
+    #supporter = models.CharField(max_length=200)
