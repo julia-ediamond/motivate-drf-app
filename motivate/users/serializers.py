ff8a7263df8a36659c87bc608056f08c06c54216
iusharnina@gmail.com <zjulia679@gmail.com>
2021-10-25 09:46:37 +1000

    Fixed hashing passwords

--- motivate/users/serializers.py
+++ motivate/users/serializers.py
@@ -1,18 +1,44 @@
+from django.contrib.auth import models
+from django.db.models import fields
 from rest_framework import serializers
 from .models import CustomUser
 
-class CustomUserSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.CharField(max_length=200)
-    email = serializers.CharField(max_length=200)
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
+            user = CustomUser(
+                email=validated_data["email"],
+                username=validated_data["username"],
+            )
+            user.set_password(validated_data["password"])
+            user.save()
+            return user
 
-class UserDetailSerializer(CustomUserSerializer):
-    def update(self, instance, validated_data):
-        instance.id = validated_data.get('id', instance.id)
-        instance.username = validated_data.get('username', instance.username)
-        instance.email = validated_data.get('email', instance.email)
-        instance.save()
-        return instance
+# class CustomUserSerializer(serializers.Serializer):
+
+
+
+    # id = serializers.ReadOnlyField()
+    # username = serializers.CharField(max_length=200)
+    # email = serializers.CharField(max_length=200)
+    # password = serializers.CharField(max_length=200)
+
+    # def create(self, validated_data):
+    #     return CustomUser.objects.create(**validated_data)
+
+# class UserDetailSerializer(CustomUserSerializer):
+#     def update(self, instance, validated_data):
+#         instance.id = validated_data.get('id', instance.id)
+#         instance.username = validated_data.get('username', instance.username)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.save()
+#         return instance
--- motivate/users/views.py
+++ motivate/users/views.py
@@ -4,17 +4,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import CustomUser
-from .serializers import CustomUserSerializer, CustomUserSerializer, UserDetailSerializer
+from .serializers import CustomUserSerializer, CustomUserSerializer, CreateUserSerializer#, #UserDetailSerializer
 from django.http import Http404
 # Create your views here.
 class CustomUserList(APIView):
     def get(self, request):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
+        #serializer = CreateUserSerializer(data=request.data)
         return Response(serializer.data)
     
     def post(self, request):
-        serializer = CustomUserSerializer(data=request.data)
+        #serializer = CustomUserSerializer(data=request.data)
+        serializer = CreateUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
